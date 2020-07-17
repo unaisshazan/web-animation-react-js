5ca9bcb676fd1a8ba56ab7995ab337183073ba05
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-07-17 19:27:32 +0500

    add

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import React,{useRef,useEffect} from 'react';
-
+import useWebAnimations from "@wellyshen/use-web-animations";
 import './App.css';
 
 function App() {
-  const element = useRef(null);
-  useEffect(() => {
-    element.current.focus();    
-  }, [])
+
+  const { ref, playState } = useWebAnimations({
+    keyframes: [
+      {transform:"translate(0,0)"},
+      {transform:"translate(100%,0)"},
+      
+
+    ],
+    timing: {
+    //  delay: 500, // Start with a 500ms delay
+      duration: 3000, // Run for 1000ms
+      iterations: Infinity, // Repeat once
+      direction: "alternate", // Run the animation forwards and then backwards
+      easing: "ease-in-out", // Use a fancy timing function
+    }
+    // onReady: ({ playState, animate, animation }) => {
+    //   // Triggered when the animation is ready to play (Google Chrome: available in v84+)
+    // },
+    // onUpdate: ({ playState, animate, animation }) => {
+    //   // Triggered when the animation enters the running state or changes state
+    // },
+    // onFinish: ({ playState, animate, animation }) => {
+    //   // Triggered when the animation enters the finished state (Google Chrome: available in v84+)
+    // },
+    // // More useful options...
+  });
   return (
     <div >
-    <div ref={element} className="target">Hello World</div>
+    <div ref={ref} className="target">Hello World</div>
     </div>
   );
 }
